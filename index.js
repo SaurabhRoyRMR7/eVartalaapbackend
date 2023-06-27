01496ffe4e3b5eb2cfb2bfbe67b5b18ba5614da7
Saurabh Roy <saurabhroyrmresm@gmail.com>
2023-06-27 17:16:24 +0530

    changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 
 app.use(express.json());
-app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(cors());
 
